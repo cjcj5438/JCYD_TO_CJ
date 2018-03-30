d5fdf2f7ae31b785c1c8bd4355f306670e8637dc
chenjing <1002931123@qq.com>
2018-03-31 01:25:35 +0800

    陈靖
    js连等关系的处理

--- 面试题/1.js
+++ 面试题/1.js
@@ -1,4 +1,7 @@
 // 1.请写出弹出值，并解释为什么
+/*
++function 中+ 号标识符 就是自调函数的写法 只是执行速度快慢的问题
+ */
 +function () {
     alert(a);
     a();
@@ -12,6 +15,13 @@
 
     alert(a)  //function () {console.log(1)}
     a();
+    /*
+        解读 line> var c=d=a;
+         首先函数分解 d=a; 这里a复制给了d ,因为d 每声明 所以d 指向 window作用域
+         var c;
+         c=d 到这步c声明在函数内部;导致外部那不到
+
+     */
     var c = d = a;
 }();
 alert(d)
--- 面试题/2.js
+++ 面试题/2.js
@@ -2,10 +2,15 @@
 this.a = 20;
 var test = {
     a: 40,
+    /*
+    ()=>{} 钩子函数的this绑定
+    是绑定到父级作用域的this上  所以此时的this.a 是20
+    在原来ES5 是 test.init().bind(this) 这样的
+     */
     init: () => {
         console.log(this.a);
         function go() {
-            // this.a = 60; 
+            // this.a = 60;
             console.log(this.a);
         }
         go.prototype.a = 50;
